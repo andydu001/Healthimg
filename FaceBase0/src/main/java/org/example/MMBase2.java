4acb4921c8f92d589cfbb2fd272833c6d2febf3b
Andy Duverneau <77910275+andydu001@users.noreply.github.com>
2025-03-08 15:03:24 -0500

    Delete FaceBase0/src/main/java/org/example/MMBase2.java

--- FaceBase0/src/main/java/org/example/MMBase2.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-
-public class MMBase2 extends FFAbase3{
-    MMBase2(){
-       // AwsBasicCredentials.create()
-    }
-}
